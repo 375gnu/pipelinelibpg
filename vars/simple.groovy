b10ce1273b7a2ef13fb554ce5a1ddd85d7ecaa0d
Hleb Valoshka <375gnu@gmail.com>
2019-02-13 13:27:56 +0300

    Update simple.groovy

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -1,10 +1,16 @@
-def call(String name) {
+def call(String name, Map config) {
   pipeline {
     agent any
     stages {
       stage('say hi') {
         steps {
           echo "hi ${name}!"
+        }
+      }
+      stage('say bye') {
+        when { expression { config.sayBye != null } }
+        steps {
+          echo "bye ${name}"
         }
       }
     }
